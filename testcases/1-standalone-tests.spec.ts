ecba51239e38372e0d28a89c541db1f4b94296ab
davidtclin <davidtclin58@gmail.com>
2026-01-20 17:10:28 +0800

    Add the aggregated test file for the standalone, cleanup and rancher integrated mode tests
    Signed-off-by: davidtclin <davidtclin58@gmail.com>

---
+++ testcases/1-standalone-tests.spec.ts
@@ -0,0 +1,57 @@
+/**
+ * Entry point for all standalone Harvester tests
+ * This file imports all test files that should run in standalone mode
+ * Tests execute in the order they are imported
+ */
+
+// Dashboard tests - Run first for initial setup and login
+import './dashboard/0_FirstTimeLogin.spec'
+import './dashboard/1_login.spec'
+import './dashboard/nav.spec'
+
+
+// Settings tests - Configure environment
+import './settings/settings.spec'
+
+// Infrastructure tests
+import './namespaces/namespaces.spec'
+import './storageclasses/storageclasses.spec'
+
+// Image tests
+import './image/images.spec'
+
+// Network tests
+import './networks/cluster-network.spec'
+import './networks/network.spec'
+
+// Volume tests
+import './volume/volumes.spec'
+
+// Virtual Machine tests
+import './virtualmachines/virtual-machine.spec'
+import './virtualmachines/advanced.spec'
+import './virtualmachines/network.spec'
+import './virtualmachines/scheduling.spec'
+import './virtualmachines/node-scheduling.spec'
+import './virtualmachines/cpu-overcommit.spec'
+import './virtualmachines/vm-migration.spec'
+
+// Backup and Snapshot tests
+import './backupAndSnapshot/vmBackup.spec'
+import './backupAndSnapshot/vmSnapshot.spec'
+import './backupAndSnapshot/volumeSnapshot.spec'
+
+// VM Settings tests
+import './VM settings/ssh-keys.spec'
+import './VM settings/cloud-config-templates.spec'
+
+// Template tests
+import './templates/template.spec'
+import './templates/advanced.spec'
+
+// Host and Support tests
+import './dashboard/hosts.spec'
+import './dashboard/support.spec'
+
+// Rancher integration tests
+import './rancher/rancher_integration.spec'
